6c754864957469d30ef5171ec1673a991f108d5c
Izzy Swart <zenerboson@gmail.com>
2019-08-13 06:49:27 +0000

    Revert "explicit bounds on sink stream"
    This reverts commit 380273250227b0ea10edb7aea68ae7a06858b5d0

--- src/protocol.rs
+++ src/protocol.rs
@@ -125,12 +125,11 @@ impl<T: Value, E: Value> Future<T, E> {
     }
 }
 
-struct SinkStream<T: Sink, U: Stream>(T, U);
+struct SinkStream<T, U>(T, U);
 
 impl<T, E, S, U> Sink for SinkStream<S, U>
 where
     S: Sink<SinkItem = T, SinkError = E>,
-    U: Stream,
 {
     type SinkItem = T;
     type SinkError = E;
@@ -146,7 +145,6 @@ where
 impl<T, E, S, U> Stream for SinkStream<S, U>
 where
     U: Stream<Item = T, Error = E>,
-    S: Sink,
 {
     type Item = T;
     type Error = E;
